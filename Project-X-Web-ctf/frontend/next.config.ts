6f478a631c14b2838bca976e830d599f6872e875
hafiz-shd <haafizshamnad@gmail.com>
2025-11-19 11:01:04 +0530

    Deployment frontend

--- Project-X-Web-ctf/frontend/next.config.ts
+++ Project-X-Web-ctf/frontend/next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverActions: {
-    bodySizeLimit: '20mb', // ✅ increase file upload limit
-  },
   experimental: {
     serverActions: {
-      bodySizeLimit: '20mb', // ✅ ensure it's picked up even in dev
+      bodySizeLimit: "20mb",   // file upload limit
+      allowedOrigins: ["*"],   // optional
     },
   },
 };
